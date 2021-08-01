78e42a57f726bcfd671c421c1b605a0bd5dc00de
paulorievrs <paulorievrsoliveira@gmail.com>
2021-08-01 17:18:53 -0300

    add the relashionship to the match model

--- app/Models/Match.php
+++ app/Models/Match.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\Uuids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Match extends Model
 {
@@ -18,4 +19,14 @@ class Match extends Model
         'home_team_score',
         'guest_team_score'
     ];
+
+    public function home_team(): BelongsTo
+    {
+        return $this->belongsTo(Team::class, 'home_team_id');
+    }
+
+    public function guest_team(): BelongsTo
+    {
+        return $this->belongsTo(Team::class, 'guest_team_id');
+    }
 }
--- app/Models/Team.php
+++ app/Models/Team.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Team extends Model
 {
@@ -16,12 +17,12 @@ class Team extends Model
       'logo'
     ];
 
-    public function home_matches()
+    public function home_matches(): HasMany
     {
         return $this->hasMany(Match::class, 'home_team_id');
     }
 
-    public function guest_matches()
+    public function guest_matches(): HasMany
     {
         return $this->hasMany(Match::class, 'guest_team_id');
     }
